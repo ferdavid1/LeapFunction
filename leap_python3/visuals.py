ef59efc06e4d65cc8e76469af5965554dd33d653
Fernando Espinosa <fespinosa@ucdavis.edu>
2019-01-15 08:24:34 -0800

    small

--- leap_python3/visuals.py
+++ leap_python3/visuals.py
@@ -12,10 +12,11 @@ screen = pygame.display.set_mode(size)
 while True:
 	pygame.draw.line(screen, WHITE, [10, 30], [10, 470], 5) # y axis
 	pygame.draw.line(screen, WHITE, [10, 470], [700, 470], 5) # x axis
-	s = 50 # number of sections
+	s = 45 # number of sections
 	l = 15 # length (mm) of sections
-	first = 235 # initial 
-	for f_region in range(10, 10+(s*l)): # frequency ranges
-		pygame.draw.line(screen, DARKGREEN, [f_region+3, first], [f_region+3+l, first], 5)
-	# pygame.draw.line(screen, DARKGREEN, [13, 235], [13+15, first], 5)
+	y = 235 # y position
+	for f_region in np.arange(15, 15+(s*l), step=l): # frequency ranges
+		print(f_region)
+		pygame.draw.line(screen, DARKGREEN, [f_region, y], [f_region+l - 5, y], 5)
+	# pygame.draw.line(screen, DARKGREEN, [13, 235], [13+15, y], 5)
 	pygame.display.flip()
