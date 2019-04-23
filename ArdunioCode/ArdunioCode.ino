7f9f96a6286391f1b610ef5b225556aa626a9cc6
Fernando Espinosa <fespinosa@ucdavis.edu>
2019-04-22 23:09:53 -0700

    small

--- ArdunioCode/ArdunioCode.ino
+++ ArdunioCode/ArdunioCode.ino
@@ -1,9 +1,13 @@
 const int button1 = 2;
 const int button2 = 3; 
+const int pot1 = A0;
+const int pot2 = A1;
 void setup() {
   // put your setup code here, to run once:
 pinMode(button1, INPUT);
 pinMode(button2, INPUT);
+pinMode(pot1, INPUT);
+pinMode(pot2, INPUT);
 Serial.begin(9600);
 }
 
@@ -11,7 +15,11 @@ void loop() {
   // put your main code here, to run repeatedly:
 int read1 = digitalRead(button1);
 int read2 = digitalRead(button2);
+int amp = analogRead(pot2);
+int interval = analogRead(pot1);
 Serial.print(read1);
 Serial.print(read2);
+Serial.print(amp);
+Serial.print(interval);
 Serial.println();
 }
